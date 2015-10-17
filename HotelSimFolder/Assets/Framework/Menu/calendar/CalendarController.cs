bdd23e0f1b8b35d768f8c87f3a4cc85bf10f4c9b
LeoQuint <leonardquintal@gmail.com>
2015-10-17 14:59:00 -0400

    meta

---
+++ HotelSimFolder/Assets/Framework/Menu/calendar/CalendarController.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CalendarController : MonoBehaviour {
+
+
+    GameObject calendarTab;
+    GameObject[] monthsHolder = new GameObject[12];
+    GameObject datePopup;
+    [SerializeField][Tooltip("Array of images for each months to display. DO NOT CHANGE SIZE VALUE!")]
+    Sprite[] ImagesOfMonth = new Sprite[12];
+	// Use this for initialization
+	void Start () {
+
+        datePopup = GameObject.FindGameObjectWithTag("UI").transform.FindChild("Popups").transform.FindChild("Calendar_Popup").gameObject;
+
+        calendarTab = GameObject.FindGameObjectWithTag("UI").transform.FindChild("Tabs").transform.FindChild("Calendar").gameObject;
+        monthsHolder[0] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("January").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[1] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("February").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[2] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("March").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[3] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("April").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[4] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("May").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[5] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("June").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[6] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("July").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[7] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("August").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[8] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("September").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[9] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("October").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[10] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("November").transform.FindChild("DayHolder").gameObject;
+        monthsHolder[11] = calendarTab.transform.FindChild("LayoutHolder").transform.FindChild("December").transform.FindChild("DayHolder").gameObject;
+        SetCalendar(2017, 1);
+	}
+    //function that displays all the dates base on the year.
+    void SetCalendar(int year, int firstDay) 
+    {
+        //Sets the displayed year at the top of the tab. 
+        calendarTab.transform.FindChild("txtYear").GetComponent<Text>().text = year.ToString();
+        date currentDate =  Calendar.getDate().deepCopy();
+        int[] numberOfdaysPerMonths = new int[12]{31,28,31,30,31,30,31,31,30,31,30,31};
+
+        
+
+
+        //12 months
+        for (int i = 0; i < 12; i++)
+        { 
+            //check for february 29th.
+            if(i == 2 && year%4 == 0)
+            {
+               numberOfdaysPerMonths[1] = 29;
+            }
+
+            if (firstDay > 1)
+            {
+                for (int k = 1; k < firstDay; k++)
+                {
+                    monthsHolder[i].transform.FindChild(k.ToString()).GetComponent<Button>().interactable = false;
+                    monthsHolder[i].transform.FindChild(k.ToString()).transform.FindChild("Text").GetComponent<Text>().text = " ";
+                }
+            }
+            int lastDay = 0;
+            bool last = false;
+            //for each days.
+            for (int j = firstDay; j <= 42; j++) 
+            {
+                if (j <= numberOfdaysPerMonths[i]+firstDay-1)
+                {
+                    int month = i;
+                    int day = (j-firstDay)+1;
+                    monthsHolder[i].transform.FindChild(j.ToString()).transform.FindChild("Text").GetComponent<Text>().text = ((j - firstDay)+1).ToString();
+                    monthsHolder[i].transform.FindChild(j.ToString()).GetComponent<Button>().onClick.AddListener(() => Display(month,day));
+                    if (!monthsHolder[i].transform.FindChild(j.ToString()).gameObject.activeInHierarchy) 
+                    {
+                        monthsHolder[i].transform.FindChild(j.ToString()).gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    if (!last)
+                    {
+                        lastDay = j-1 ;
+                        last = true;
+                    }
+                    monthsHolder[i].transform.FindChild(j.ToString()).gameObject.SetActive(false);
+                }
+
+            }
+            //calculate the first day for the next month.
+            firstDay = (lastDay % 7)+1;
+            if (firstDay == 0)
+            {
+                firstDay = 1;
+            }
+          
+        }
+    }
+    //activates the Popup for date display. 
+    public void Display(int month, int day) 
+    {
+        datePopup.SetActive(true);
+        datePopup.transform.FindChild("txtDate").GetComponent<Text>().text = ((months)month).ToString() + " " + day.ToString();
+        datePopup.transform.FindChild("IMG_Month").GetComponent<Image>().sprite = ImagesOfMonth[month];
+    }
+}
