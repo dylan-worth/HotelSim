2cbc3f202192f377ecff6b925e2080869fbce2eb
LeoQuint <leonardquintal@gmail.com>
2015-11-08 16:46:33 -0500

    Fixed starting menu. Added framework for userID/Password.

--- HotelSimFolder/Assets/Framework/Utilities/MainMenuUI.cs
+++ HotelSimFolder/Assets/Framework/Utilities/MainMenuUI.cs
@@ -1,70 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MainMenuUI : MonoBehaviour {
 
-	bool moveLogo = true;
-	bool moveInstitute = true;
-	bool moveNewGame = true;
+    Dictionary<int, string> loggingInfo = new Dictionary<int,string>();
+	
+    void Start()
+    {
+        loggingInfo.Add(100975584, "password");
+    }
 
-	public Transform logoEnd;
-	public Transform instituteEnd;
-	public Transform newGameEnd;
-
-	public GameObject logo;
-	public GameObject institute;
-	public GameObject newGame;
-
-	Transform startLogo;
-	Transform startInstitute;
-	Transform startButton;
-
-	float startTime;
-	float journeyLength = 3.66f;
-
-	void Start()
-	{
-		startTime = Time.time;
-		startLogo = logo.transform;
-		startButton = newGame.transform;
-		startInstitute = institute.transform;
-		Invoke ("StopMove", 3.66f);
-	}
-
-	void Update()
-	{
-		if (moveLogo)
-			MoveTheLogo ();
-		if (moveInstitute)
-			MoveTheInstitute ();
-		if (moveNewGame)
-			MoveTheNewGame ();
-	}
-	void MoveTheLogo()
-	{
-		float fracJourney = (Time.time - startTime) / journeyLength;
-		logo.transform.position = Vector3.Lerp(startLogo.position, logoEnd.position, fracJourney);
-	}
-	void MoveTheInstitute()
-	{
-		float fracJourney = (Time.time - startTime) / journeyLength;
-		institute.transform.position = Vector3.Lerp(startInstitute.position, instituteEnd.position, fracJourney);
-	}
-	void MoveTheNewGame()
-	{
-		float fracJourney = (Time.time - startTime) / journeyLength;
-		newGame.transform.position = Vector3.Lerp(startButton.position, newGameEnd.position, fracJourney);
-	}
-	void StopMove()
-	{
-		moveLogo = false;
-		moveInstitute = false;
-		moveNewGame = false;
-	}
 	public void LoadMainLevel()
 	{
-		Application.LoadLevel ("TestScene");
+		Application.LoadLevel ("Hotel");
+        Debug.Log("click");
 	}
+
+    public void StudentLogging() 
+    {
+        GameObject logPan = GameObject.FindGameObjectWithTag("UI").transform.FindChild("MainUI").transform.FindChild("LoggingPanel").gameObject;
+        int studentID =  int.Parse( logPan.transform.FindChild("Input_ID").gameObject.GetComponent<InputField>().text);
+        string studentPass = logPan.transform.FindChild("Input_Password").gameObject.GetComponent<InputField>().text;
+
+        if (loggingInfo.ContainsKey(studentID))
+        {
+            if (studentPass == loggingInfo[studentID]) 
+            {
+                LoadMainLevel();
+            }
+        }
+        else 
+        {
+            logPan.transform.FindChild("txt_Errors").GetComponent<Text>().text = "Student ID or password incorrect.";
+        }
+
+      
+    }
 }
 
 
